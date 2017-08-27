4869af8acc9914d10f465da87dfb5ca502f7bff7
chrislin89 <chrislin.317@hotmail.com>
2017-08-27 20:10:52 +0800

    Initial Commit

---
+++ USeek/USeek.h
@@ -0,0 +1,19 @@
+//
+//  USeek.h
+//  USeek
+//
+//  Created by Chris Lin on 8/27/17.
+//  Copyright © 2017 USeek. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for USeek.
+FOUNDATION_EXPORT double USeekVersionNumber;
+
+//! Project version string for USeek.
+FOUNDATION_EXPORT const unsigned char USeekVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <USeek/PublicHeader.h>
+
+
